8d34862fbab67a2e654d6f5bb5a2e1d88dce729e
Riju Abraham <rijuabraham@tamu.edu>
2022-10-10 14:46:53 -0500

    updated webserver and request to include functionalities

---
+++ generateIP.cpp
@@ -0,0 +1,23 @@
+
+
+#include <iostream>
+#include <random>
+#include <string> 
+
+using namespace std;
+
+
+string generateIP (){
+
+    string IP = "";
+
+    for (int i = 0; i < 3; i++)
+    {
+        int val = rand() % 255;
+        IP+= to_string(val);
+    }
+    
+
+    return IP;
+
+}
---
+++ webserver.cpp
@@ -0,0 +1,30 @@
+
+
+#include <iostream>
+
+using namespace std;
+
+class webServer {
+
+    public:
+        string status = "Avaialble";
+        string id = NULL;
+
+    
+        void processreq(request req) {
+
+            this->id = "Busy";
+            
+            cout<<this->id<<" Processed request: "<<request.getinIP()<<" to "<<request.getinIP()<<endl;
+
+        }
+    
+
+    
+
+
+
+
+
+
+}
